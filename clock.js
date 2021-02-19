9db2da53858f3920d37f7e11a4a760b0a744b995
YuyiFan325 <sirius0325yj@gmail.com>
2021-02-19 11:15:00 -0500

    2.19

--- clock.js
+++ clock.js
@@ -1,43 +1,31 @@
 $(document).ready(function(){
+	$('#fullpage').fullpage();
+});
+
+
+
+
+$(document).ready(function(){
 	$(".header").click(function(){
-		$(".header").animate({
-			opacity: "0.3",
-			height: "10px",
-			width: "15px",
-		}),
-		$(".header").toggleClass(".header");
+		$("body").toggleClass("dark");
+		$(".eyeclock").fadeToggle(3000);
 	});
 });
 
 $(document).ready(function(){
 	$(".header").click(function(){
-		$(".header").animate({
-			opacity: "0.3",
-			height: "10px",
-			width: "15px",
-		});
+		$("main1").fadeToggle("active");
 	});
 });
-
 
 $(document).ready(function(){
 	$("button").click(function(){
-		$(".main1").animate({
+		$("#main1").animate({
 			left: "250px",
 			opacity: "0.2",
 			height: "150px",
 			width: "150px"
 		});
-	});
-});
-
-$(document).ready(function(){
-	$("rightArrow").click(function(){
-		var currentSlide = $(".header.active");
-		var nextSlide = currentSlide.next();
-
-		currentSlide.fadeOut(300).removeClass("active");
-		nextSlide.fadeIn(300).addClass("active");
 	});
 });
 
@@ -52,4 +40,20 @@ $(document).ready(function(){
 	});
 });
 
+
+$(document).ready(function(){
+	$("p").click(function(){
+		$("p").toggleClass("active");
+	});
+});
+
+$(document).ready(function(){
+	$("rightArrow").click(function(){
+		var currentSlide = $(".header.active");
+		var nextSlide = currentSlide.next();
+
+		currentSlide.fadeOut(300).removeClass("active");
+		nextSlide.fadeIn(300).addClass("active");
+	});
+});
 */
